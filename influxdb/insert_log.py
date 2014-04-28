957192841145b67c77dfdd260bbc31089d14b4df
Bill Allen <photo.allen@gmail.com>
2014-04-27 19:25:12 -0600

    Desperately needed to add times to make the data queryable.  Assumes a 250ms write delay between each 10s reading.

--- influxdb/insert_log.py
+++ influxdb/insert_log.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python
 
 import sys
+import time
+from datetime import datetime, timedelta
 from influxdb import client as influxdb
 
+def parseTimestamp(ts):
+	year = int(ts[0:4])
+	month = int(ts[4:6])
+	day = int(ts[6:8])
+	hour = int(ts[8:10])
+	minute = int(ts[10:12])
+	second = int(ts[12:14])
+
+	return datetime(year, month, day, hour, minute, second)
+
 if __name__ == "__main__":
-	f = open(sys.argv[1], 'r')
+	endTimestamp = sys.argv[1] # YYYYMMDDhhmmss
+
+	if len(endTimestamp) != 14:
+		print 'Invalid timestamp format.'
+		sys.exit(1)
+
+	delta = timedelta(seconds=10, milliseconds=250) # attempting to account for the write time
+	endTime = parseTimestamp(endTimestamp)
+	f = open(sys.argv[2], 'r')
 	db = influxdb.InfluxDBClient('localhost', 8086, 'root', 'root', 'plant')
 	points = []
 
@@ -16,10 +36,19 @@ if __name__ == "__main__":
 		moist = float(moist)
 		points.append([temp,hum,dew,moist])
 
+	points.reverse() # so we can start with the endtime
+
+	lastTime = endTime
+
+	for point in points:
+		point.append(time.mktime(lastTime.timetuple()))
+		lastTime = lastTime - delta
+
 	data = [{
 		'points':points,
 		'name':'readings',
-		'columns':['temp','humidity','dew','moisture']
+		'columns':['temp','humidity','dew','moisture','time']
 	}]
+
 	db.write_points(data)
 	f.close()
